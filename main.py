1fc39d4821c99dab652e7c50ea83e65d3f2d512f
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2020-03-25 19:49:42 +0100

    Added pressure figure

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 import ms5837
 import RPi.GPIO as GPIO
-import math, sys, os, time, threading
+import math
+import sys
+import os
+import time
+import threading
 from timeit import default_timer as timer
 
-#from matplotlib.pyplot import *
+from matplotlib.pyplot import *
 
-#sudo apt-get install python3-smbus
+#sudo apt-get install python-smbus
 # or
-#sudo pip3 install smbus
+#sudo pip install smbus
 # if it did not work.
 # Raspberry Pi configuration :
 #sudo nano /boot/config.txt
@@ -16,13 +20,13 @@ from timeit import default_timer as timer
 #dtparam=i2c_arm=on
 #dtoverlay=pwm-2chan,pin=12,func=4,pin2=13,func2=4
 # Then reboot and
-#sudo python3 example.py
+#sudo python example.py
 
 # Parameters
 ###############################################################################
-p_delta_max = 0.02 # In bar
-p_delta_danger_max = 0.025 # In bar
-p_delta_danger_min = -0.005 # In bar
+p_delta_max = 20 # In mbar
+p_delta_danger_max = 25 # In mbar
+p_delta_danger_min = -5 # In mbar
 breath_freq = 30 # In cycles/min
 inspi_ratio = 1.0/3.0
 #trim PWM value start, end depending on balloon size...
@@ -30,7 +34,7 @@ pwm_ns_min = 1000000
 pwm_ns_max = 2000000
 ###############################################################################
 
-# Software PWM init
+# Software PWM init (for buzzer and status LED)
 GPIO.setwarnings(False)	
 GPIO.setmode(GPIO.BCM)
 buz_pin = 26
@@ -42,7 +46,7 @@ GPIO.setup(led_pin,GPIO.OUT)
 led_pwm = GPIO.PWM(led_pin, 1000)
 led_pwm.start(100)
 
-# Hardware PWM init
+# Hardware PWM init (for servos)
 os.system('echo 0 > /sys/class/pwm/pwmchip0/export')
 os.system('echo 1 > /sys/class/pwm/pwmchip0/export')
 os.system('echo 20000000 > /sys/class/pwm/pwmchip0/pwm0/period')
@@ -57,13 +61,10 @@ os.system('echo 1 > /sys/class/pwm/pwmchip0/pwm1/enable')
 sensor = ms5837.MS5837_02BA()
 #sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, bus=0) # Specify model
 #and bus
-
-# We must initialize the sensor before reading it
 if not sensor.init():
     print('Sensor could not be initialized')
     exit(1)
 
-# We have to read values from sensor to update pressure and temperature
 if not sensor.read():
     print('Sensor read failed!')
     exit(1)
@@ -73,15 +74,13 @@ cycle_duration = 1.0/breath_freq_converted
 inspi_duration = inspi_ratio*cycle_duration
 expi_duration = cycle_duration-inspi_duration
 
-#fig = figure('Breath')
-#scale = 10
-#offsetx = 0
-#offsety = 0
-
 t = timer()
 t_prev = t
+t0 = t
 t_cycle_start = t
 p = sensor.pressure()
+p_prev = p
+p0 = p
 p_cycle_start = p
 inspi_end = False
 inspi_duration_estim = inspi_duration
@@ -90,23 +89,32 @@ cycle_duration_estim = cycle_duration
 
 pwm_ns = pwm_ns_min
 
-# Should check p behavior (too high, too low, should compute p_delta_meas_min, p_delta_meas_max)...
+fig = figure('Pressure')
+#clf()
+#axis('square')
+scalex = 10
+scaley = 1.5*p_delta_max
+offsetx = -scalex
+offsety = p0
 
+# Should check p behavior (too high, too low, should compute p_delta_meas_min,
+# p_delta_meas_max)...
 while True:
 
-    #pwm_ns = 1000000*(1.5-0.5*math.cos(2*math.pi*(t-t_cycle_start)/cycle_duration_estim))
+    #pwm_ns =
+    #1000000*(1.5-0.5*math.cos(2*math.pi*(t-t_cycle_start)/cycle_duration_estim))
 
     # Should check bounds of pwm...
     if (t-t_cycle_start < inspi_duration_estim):
         pwm_ns = pwm_ns_min+(pwm_ns_max-pwm_ns_min)*(t-t_cycle_start)/inspi_duration_estim
     else:
         pwm_ns = pwm_ns_max-(pwm_ns_max-pwm_ns_min)*(t-t_cycle_start-inspi_duration_estim)/expi_duration_estim
-    pwm_ns=min(pwm_ns_max, max(pwm_ns_min, pwm_ns))
+    pwm_ns = min(pwm_ns_max, max(pwm_ns_min, pwm_ns))
 
     #if (p-p_cycle_start < p_delta_max) and !inspi_end:
     #    pwm_ns = 1000000*(1+(t-t_cycle_start)/inspi_duration_estim)
     #    if (t-t_cycle_start > inspi_duration_estim):
-    #        print('!!! WARNING !!!')
+    #        print('!!!  WARNING !!!')
     #        inspi_end = True
     #        inspi_duration_estim = inspi_duration
     #        expi_duration_estim = expi_duration
@@ -127,12 +135,12 @@ while True:
 
     # Test...
     buz_pwm.ChangeFrequency(math.trunc(pwm_ns/1000.0))
-    led_pwm.ChangeDutyCycle(math.trunc(100*min(1.0, (t-t_cycle_start)/cycle_duration_estim)))
+    led_pwm.ChangeDutyCycle(min(100, max(0, math.trunc(100*min(1.0, (t-t_cycle_start)/cycle_duration_estim)))))
     
-    #clf()
-    #axis('square')
-    #axis([-scale+offsetx,scale+offsetx,-scale+offsety,scale+offsety])
-    #plot(M[0].flatten(),M[1].flatten(),'b')
+    axis([-scalex+offsetx,scalex+offsetx,-scaley+offsety,scaley+offsety])
+    plot([t_prev-t0,t-t0], [p_prev,p], 'b')
+    pause(0.001)
+    offsetx = offsetx+t-t_prev
  
     if sensor.read():
         print('P: %0.1f mbar \tT: %0.2f C ') % (sensor.pressure(), sensor.temperature()) 
@@ -142,6 +150,7 @@ while True:
     
     t_prev = t
     t = timer()
+    p_prev = p
     p = sensor.pressure()
     if (t-t_cycle_start > cycle_duration_estim):
         t_cycle_start = t
